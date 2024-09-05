42ad590b17102b1a05c908a3b1fa09e679cc7b43
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-09-06 00:06:25 +0530

    Add files via upload

---
+++ Loop/For.py
@@ -0,0 +1,17 @@
+#tuple=(1,4,9,25,36,49,64,81,100)
+#x=100
+#for i in tuple:
+ #   if x==i:
+ #       print("Num Is Found",i)
+  #  else:
+    #    print("Not Found")
+
+#tuple=(1,4,9,25,36,49,64,81,100)
+#x=100
+#idx=0
+#for i in tuple:
+  #  if i==x:
+    #    print("Num Is Found",idx)
+    #idx+=1
+
+    
---
+++ Loop/Range_Function.py
@@ -0,0 +1,74 @@
+#Range()
+
+for i in range(10):  #stop
+    print(i)
+
+for q in range(2,7): #start stop
+    print(q)
+  
+for w in range(2,10,2): #start stop increment
+    print(w)
+
+
+#for r in range(100,0,-1):
+  #  print(r)
+
+
+#pass
+for E in range(5):
+    pass         #empty null statement
+print("working...")
+
+#q1
+#n=int(input("Enter limit"))
+#s=0
+#i=1
+#while(i<=n):
+    #s=s+i
+    #i+=1
+    #print("sum of n number=",s)
+    
+
+#q2
+n=6
+s=0
+for i in range(1,n+1):
+     s=s+i
+print(s)
+
+#factorial first n numbers
+n=5
+f=1
+for z in range(1,n+1):
+    f*=z
+    print(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
