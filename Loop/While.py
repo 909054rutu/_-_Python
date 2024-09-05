51ee20d0c04cbac32b0a4c070903d5783bd67df2
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-09-05 10:30:11 +0530

    Add files via upload

---
+++ Loop/While.py
@@ -0,0 +1,43 @@
+#loop
+#count=1
+#while(count<101):
+#    print(count)
+#    count+=1
+
+#q1
+#count=100
+#while(count>0):
+#   print(count)
+#   count-=1
+    
+#q2
+#n=int(input("Enter number"))
+#i=1
+#while(i<=10):
+#    print(i*n)
+#  i+=1
+
+#q3
+#list=[1,2,3,56,8,21,34,5,78,90]
+#idx=0
+#while idx<len(list):
+  #  print(list[idx]) #list[0],list[1],list[2]
+   # print(idx)
+    # idx+=1
+
+#q4
+s_no=int(input("Enter Number"))
+tup1=(99,94,75,77)
+i=0
+while i<len(tup1):
+    if tup1[i]==s_no:
+        print("found",i)
+    else:
+            print("not found")
+    i+=1
+
+
+
+
+
+            
